32ce6fd378c149c88e309104d2d217946de93375
Michael Ord <michael.ord@hotmail.co.uk>
2019-08-10 18:12:21 +0100

    ipaddress parced to guess CIDR, and COnverting ipaddress to byte representation string

--- IPFiltering.Library.Tests/IPFilterTests.cs
+++ IPFiltering.Library.Tests/IPFilterTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Net;
 
 namespace IPFilter.Library.Tests
 {
@@ -6,9 +7,40 @@ namespace IPFilter.Library.Tests
     public class IPFilterTests
     {
         [Test]
-        public void Test1()
+        public void IPNetwork_Can_Parse_IPStrings([Values("192.168.1.3", "192.168.1.3/24")] string IPInput)
         {
-            Assert.Pass();
+            // Act
+            int cIDR = IPFiltering.Library.IPFilter.ParseAddressToGetCIDR(IPInput);
+
+            // Assert
+            Assert.AreEqual(24, cIDR);
+        }
+
+        [Test]
+        public void IPNetwork_Converts_To_Bytes([Values("192.168.1.3", "192.168.1.3/24")] string IPInput)
+        {
+            var ipStrings = IPInput.Split("/");
+            // Act
+            var IpBytesString = IPFiltering.Library.IPFilter.ConvertToBytesString(ipStrings[0]);
+
+            // Assert
+            Assert.AreEqual("11000000101010000000000100000011", IpBytesString);
+        }
+
+
+        [Test]
+        public void Find_IP_Address_In_List_True([Values("192.168.1.3", "192.168.1.3/24")] string IPInput)
+        {
+            // Arrange
+            var ipStrings = IPInput.Split("/");
+            var IPBytesString = IPFiltering.Library.IPFilter.ConvertToBytesString(ipStrings[0]);
+            var cidr = IPFiltering.Library.IPFilter.ParseAddressToGetCIDR(IPInput);
+
+            //Act
+            bool result = IPFiltering.Library.IPFilter.FindAddressInList(IPBytesString, cidr);
+
+            // Assert
+            Assert.IsTrue(result);
         }
     }
 }
--- IPFiltering.Library/IPFilter.cs
+++ IPFiltering.Library/IPFilter.cs
@@ -1,8 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace IPFiltering.Library
 {
     public class IPFilter
     {
+        private Dictionary<string, int> IpAddressList = new Dictionary<string, int>();
+        public static int ParseAddressToGetCIDR(string iPInput)
+        {
+            if (iPInput.Split("/").Length == 2)
+                return IPNetwork.Parse(iPInput).Cidr;
+            else
+                return IPNetwork.Parse(iPInput, CidrGuess.ClassFull).Cidr;
+        }
+
+        public static string ConvertToBytesString(string ipInput)
+        {
+            return String.Join("", ( // join segments
+                    ipInput.Split('.').Select( // split segments into a string[]
+
+                    // take each element of array, name it "x",
+                    //   and return binary format string
+                    x => Convert.ToString(Int32.Parse(x), 2).PadLeft(8, '0')
+
+                    // convert the IEnumerable<string> to string[],
+                    // which is 2nd parameter of String.Join
+                    )).ToArray());
+        }
+
+        public static bool FindAddressInList(string iPBytesString, int cidr)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
